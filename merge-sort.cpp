b76806ad1b6c6b085144caddddef5e1d98b76f31
padhmasrib <143668806+padhmasrib@users.noreply.github.com>
2025-02-06 18:41:57 -0500

    Create merge-sort.cpp
    Trying to add a file to GitHub repository

---
+++ merge-sort.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+void merge_sort (int arr[]){
+
+
+}
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello merge-sort" << endl;    
+
+}
