1166cf06b48e4305ac10a204ec2304e6e67301c8
padhmasrib <143668806+padhmasrib@users.noreply.github.com>
2025-02-07 19:49:18 -0500

    changed run for different array sizes

--- run-merge-sort.sh
+++ run-merge-sort.sh
@@ -2,6 +2,16 @@
 ## need to create a Makefile and use the make command instead of the following
 g++ merge-sort.cpp -o merge-sort
 
-## redirects time output also to merge-sort-answer
-(time ./merge-sort 12) > merge-sort-answer 2>&1
+## redirects time output also to merge-sort-answer for various array sizes
+{ 
+    (time ./merge-sort 10)
+    (time ./merge-sort 100)
+    (time ./merge-sort 1000)
+    (time ./merge-sort 10000)
+    (time ./merge-sort 100000)
+    (time ./merge-sort 1000000)
+    (time ./merge-sort 10000000)
+    (time ./merge-sort 100000000)
+    (time ./merge-sort 1000000000)
+} > merge-sort-answer 2>&1
 
